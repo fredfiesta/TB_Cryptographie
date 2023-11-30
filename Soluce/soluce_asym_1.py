9f057a9eb5fc2207098b1d0d4e019fe363e2ba88
Stéphane Bongard <71258875+fredfiesta@users.noreply.github.com>
2023-11-30 10:11:29 +0100

    Update soluce_asym_1.py
    Correction du texte

--- Soluce/soluce_asym_1.py
+++ Soluce/soluce_asym_1.py
@@ -1,9 +1,9 @@
 # Solution - Exercice 1
 # Votre code
-# Generate private
+# Génération de la clé privée
 commande1='openssl genpkey -algorithm RSA -out Fichiers/Asymetrique/Exercice1.pem'
 ef.func_cmd_generate_rsa(commande1)
 
-# Generate public
+# Génération de la clé publique
 commande2='openssl pkey -in Fichiers/Asymetrique/Exercice1.pem -out Fichiers/Asymetrique/Exercice1.pub -pubout'
 ef.func_cmd_extract_rsa(commande2)
